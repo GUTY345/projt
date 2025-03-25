01196c22cf354701c4aa286280524b01bc30b144
GUTY345 <Mathachai363@gmail.com>
2025-03-25 17:00:38 +0700

    update chat

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './styles/theme';
-import App from './app';
-import { AuthProvider } from './contexts/AuthContext';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';  // Fixed import syntax
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
